b48f01e5d7a592692d072229c754be4784e081a5
Kareem Mohammed <109914324+karemsaeed21@users.noreply.github.com>
2024-08-06 03:04:00 +0300

    [71] [Time Beats: 92.53%] [Memory Beats: 59.72%] - LeetPush

---
+++ 71-Simplify-Path.py
@@ -0,0 +1,10 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        parts = path.split('/')
+        stack = deque()
+        for part in parts:
+            if part == '' or part == '.': continue
+            elif part == '..':
+                if stack: stack.pop()
+            else:stack.append(part)
+        return '/' + '/'.join(stack)
