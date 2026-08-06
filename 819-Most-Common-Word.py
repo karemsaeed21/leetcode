57203815b866e9bd2390316e45052f66460dd14a
Kareem Mohammed <109914324+karemsaeed21@users.noreply.github.com>
2026-08-06 03:57:45 +0300

    [819] [Time Beats: 100.00%] [Memory Beats: 6.10%] - LeetPush

--- 819-Most-Common-Word.py
+++ 819-Most-Common-Word.py
@@ -1,17 +1,11 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        new_paragraph = ""
-        for i in paragraph:
-            if i.isalpha() or i == " ":
-                new_paragraph += i.lower()
-        listed_para = list(new_paragraph.split(" "))
-        co = listed_para.count(listed_para[0])
-        word = listed_para[0]
-        for i in listed_para:
-            if i in banned:
-                continue
-            else:
-                if (listed_para.count(i) > co):
-                    co = listed_para.count(i)
-                    word = i
-        return str(word)
+        normalized_str = re.sub(r'[^\w\s]', ' ', paragraph).lower()
+        
+        words = normalized_str.split()
+        
+        banned_set = set(banned)
+        
+        counts = Counter(word for word in words if word not in banned_set)
+        
+        return counts.most_common(1)[0][0]
